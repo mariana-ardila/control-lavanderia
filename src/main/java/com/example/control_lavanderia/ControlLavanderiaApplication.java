34663f477269eef20045c294482c1abfc0062eac
mariana-ardila <desarrolladoraardila@gmail.com>
2026-04-11 14:32:58 -0500

    Creación del repositorio

---
+++ src/main/java/com/example/control_lavanderia/ControlLavanderiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.control_lavanderia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlLavanderiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlLavanderiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/control_lavanderia/ControlLavanderiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.control_lavanderia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlLavanderiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
